a88b39796e45bd4f89300f7a15557193ecc36122
Ayush Dhiman <ayushdhiman0107@gmail.com>
2022-07-02 22:17:03 +0530

    SDC Client Frontend Initial Commit

---
+++ src/components/post/Post.jsx
@@ -0,0 +1,26 @@
+import "./post.css";
+import { Link } from "react-router-dom";
+
+export default function Post({ post }) {
+  const PF = "https://sdc-blog.herokuapp.com/images/";
+  return (
+    <div className="post">
+      {post.photo && <img className="postImg" src={PF + post.photo} alt="" />}
+      <div className="postInfo">
+        <div className="postcard">
+          {post.categories.map((c) => (
+            <span className="postCat">{c.name}</span>
+          ))}
+        </div>
+        <Link to={`/post/${post._id}`} className="link" style={{marginTop:'20px'}}>
+          <span className="postTitle"  style={{color:'black'}}>{post.title}</span>
+        </Link>
+        <hr />
+        <span className="postDate"  style={{color:'white', fontSize:'15px', backgroundColor:'black', padding:'10px'}}>
+          {new Date(post.createdAt).toDateString()}
+        </span>
+      </div>
+      <p className="postDesc">{post.desc}</p>
+    </div>
+  );
+}
---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,63 @@
+import axios from "axios";
+import { useContext, useRef } from "react";
+import { Link } from "react-router-dom";
+import { Context } from "../../context/Context";
+import "./login.css";
+
+export default function Login() {
+  const userRef = useRef();
+  const passwordRef = useRef();
+  const { user, dispatch, isFetching, error } = useContext(Context);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    dispatch({ type: "LOGIN_START" });
+    try {
+      const res = await axios.post("/auth/login", {
+        username: userRef.current.value,
+        password: passwordRef.current.value,
+      });
+      dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
+    } catch (err) {
+      dispatch({ type: "LOGIN_FAILURE" });
+    }
+  };
+
+  return (
+    <div className="login">
+      <span className="loginTitle">Login</span>
+      <form className="loginForm" onSubmit={handleSubmit}>
+        <label>Username</label>
+        <input
+          type="text"
+          className="loginInput"
+          placeholder="Enter your username..."
+          ref={userRef}
+          style={{ color: "white" }}
+        />
+        <label>Password</label>
+        <input
+          type="text"
+          className="loginInput"
+          placeholder="Enter your password..."
+          ref={passwordRef}
+          style={{ color: "white" }}
+        />
+        {error ? (
+          <div style={{ marginTop: 5 }}>
+            <em style={{ color: "red", fontWeight: "bold" }}>
+              Please Check the Details Again !
+            </em>
+          </div>
+        ) : null}
+        <button className="loginButton" type="submit" disabled={isFetching}>
+          Login
+        </button>
+        <button className="loginRegisterButton">
+          <Link className="link" to="/register">
+            Register
+          </Link>
+        </button>
+      </form>
+    </div>
+  );
+}
